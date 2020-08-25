b4ea0e8af9fac70cde2536c81ed636a630b4c2bf
dany <78528291@163.com>
2020-08-25 12:31:40 +0800

    rebuilding site Tue Aug 25 12:31:40 CST 2020

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 function ChangeImgSrcWithoutId() {
     var arrImg = document.images;
     for (var i = 0; i < arrImg.length; i++) {
-        if (arrImg[i].getAttribute("src") != undefined && arrImg[i].getAttribute("title") != undefined) {
+        if (arrImg[i].getAttribute("src") != undefined && arrImg[i].getAttribute("title") != undefined && arrImg[i].getAttribute("src") == arrImg[i].getAttribute("title")) {
             arrImg[i].src = "../" + arrImg[i].getAttribute("src");
         }
     }
