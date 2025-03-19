1147490fd06ec4b7f592548b839ca1de9db7ff45
modan <78528291@qq.com>
2025-03-19 18:46:42 +0800

    rebuilding site 2025年 03月 19日 星期三 18:46:42 CST

--- js/custom.js
+++
@@ -1,9 +0,0 @@
-function ChangeImgSrcWithoutId() {
-    var arrImg = document.images;
-    for (var i = 0; i < arrImg.length; i++) {
-        if (arrImg[i].getAttribute("src") != undefined && arrImg[i].getAttribute("title") != undefined && arrImg[i].getAttribute("src") == arrImg[i].getAttribute("title")) {
-            arrImg[i].src = "../" + arrImg[i].getAttribute("src");
-        }
-    }
-}
-ChangeImgSrcWithoutId();//调用
